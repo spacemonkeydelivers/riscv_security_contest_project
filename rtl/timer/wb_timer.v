4518ad1c6d480ea5aa4bce94fa4a8a3a3e88ac64
Alex White <space.monkey.delivers@gmail.com>
2019-07-24 18:27:02 +0300

    Reworked timer to be compliant to WB

--- rtl/timer/wb_timer.v
+++ rtl/timer/wb_timer.v
@@ -4,79 +4,68 @@ module wb_timer
 #(
    parameter WB_DATA_WIDTH = 32,
    parameter WB_ADDR_WIDTH = 32,
-   parameter WB_ADDR_START = 32'h00000000
+   parameter WB_SEL_WIDTH  = 4
 )
 (
-   input  wire        clk_i,
-   input  wire        rst_i,
-   input  wire [WB_DATA_WIDTH - 1:0] wb_dat_i,
+   input  wire                       clk_i,
+   input  wire                       rst_i,
    input  wire [WB_ADDR_WIDTH - 1:0] wb_addr_i,
-   input  wire        wb_we_i,
-   input  wire        wb_cyc_i,
-   output wire        irq_o,
-   output wire [WB_DATA_WIDTH - 1:0] wb_dat_o
+   input  wire [WB_DATA_WIDTH - 1:0] wb_data_i,
+   input  wire                       wb_we_i,
+   input  wire [WB_SEL_WIDTH - 1:0]  wb_sel_i,
+   input  wire                       wb_stb_i,
+   input  wire                       wb_cyc_i,
+   output wire                       wb_ack_o,
+   output wire [WB_DATA_WIDTH - 1:0] wb_data_o,
+   output wire                       timer_irq_o
 );
-   // granularity of access 
-   localparam WB_TIMER_ACCESS_SIZE = $clog2(WB_DATA_WIDTH);
-
-   // address to set timer threshold
-   localparam WB_TIMER_SET_THRESHOLD = WB_ADDR_START;
-   // address to set control for timer
-   localparam WB_TIMER_SET_CONTOL    = WB_TIMER_SET_THRESHOLD + WB_TIMER_ACCESS_SIZE;
-   // address to clear timer
-   localparam WB_TIMER_SET_CLEAR     = WB_TIMER_SET_CONTOL + WB_TIMER_ACCESS_SIZE;
-   localparam WB_ADDR_END            = WB_TIMER_SET_CLEAR + WB_TIMER_ACCESS_SIZE;
-
    // irq pin, raised to HIGH if current time exceeds the threshold
    reg irq = 0;
-   assign irq_o = irq;
+   assign timer_irq_o = irq;
 
+   // ack signal for wishbone
+   reg ack = 0;
+   assign wb_ack_o = ack;
+   // if timer is running
+   reg timer_started = 0;
    // contains current time
    reg [WB_DATA_WIDTH - 1:0] current_time = 0;
    // contains threshold tome
    reg [WB_DATA_WIDTH - 1:0] threshold_time = 0;
-   // contains contol logic for the timer
-   reg [WB_DATA_WIDTH - 1:0] control = 0;
 
-   wire timer_accessed = ((wb_addr_i >= WB_ADDR_START) && (wb_addr_i < WB_ADDR_END));
-   wire threshold_set  = (wb_addr_i == WB_TIMER_SET_THRESHOLD);
-   wire contol_set     = (wb_addr_i == WB_TIMER_SET_CONTOL);
-   wire clear_set      = (wb_addr_i == WB_TIMER_SET_CLEAR);
+   // TODO: WB_ACK_O
 
    always @ (posedge clk_i)
    begin
       if (rst_i)
       begin
+         ack <= 0;
          irq <= 0;
          current_time <= 0;
          threshold_time <= 0;
-         control <= 0;
+         timer_started <= 0;
       end
       else
       begin
-         if (control[`WB_TIMER_ENABLED])
+         if (timer_started)
          begin
             current_time <= current_time + 1;
             irq <= (current_time >= threshold_time);
          end
-         if (wb_cyc_i && wb_we_i && timer_accessed)
+         if (wb_cyc_i)
          begin
-            if (threshold_set)
+            ack <= 1;
+            if (wb_we_i)
             begin
-               threshold_time <= wb_dat_i;
+               threshold_time <= wb_data_i;
+               timer_started <= 1;
             end
-            if (contol_set)
-            begin
-               control <= wb_dat_i;
-            end
-            if (clear_set)
+            else
             begin
                current_time <= 0;
-               irq <= 0;
             end
          end
+         ack <= 0;
       end
    end
-
-
 endmodule
