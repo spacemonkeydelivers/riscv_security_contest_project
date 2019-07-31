b576ae786d93edcd578ad7176ef1c39cb5e40842
Alex White <space.monkey.delivers@gmail.com>
2019-07-31 22:24:54 +0300

    Small fix for sample tests

--- tests/basic_1.cpp
+++ tests/basic_1.cpp
@@ -16,18 +16,6 @@ int main(int argc, char **argv)
     rv_soc.reset();
     rv_soc.tick(20);
 
-    rv_soc.dumpRam(0, 20);
-    rv_soc.doMemOp(0x10, RV_SOC::MemOpcode::READB);
-    rv_soc.doMemOp(0x11, RV_SOC::MemOpcode::WRITEB);
-    rv_soc.dumpRam(0, 20);
-
-    printf("\n");
-    rv_soc.doMemOp(0x0, RV_SOC::MemOpcode::WRITEB, 0x11335577);
-    rv_soc.doMemOp(0x4, RV_SOC::MemOpcode::WRITEH, 0x22446688);
-    rv_soc.doMemOp(0x8, RV_SOC::MemOpcode::WRITEW, 0xAABBAABB);
-    rv_soc.doMemOp(0xc, RV_SOC::MemOpcode::WRITEB, 0x065560);
-    rv_soc.dumpRam(0, 20);
-
     return 0;
 }
 
