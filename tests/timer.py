883884e318f44a33d182927306012a30739d616d
Alexey Baturo <baturo.alexey@gmail.com>
2019-08-02 10:32:02 +0300

    Fixed compile_to_asm script and improved timer test

--- tests/timer.py
+++ tests/timer.py
@@ -6,10 +6,6 @@ import soc
 
 soc = soc.RiscVSoc(libbench, 'memtest_trace.vcd', True)
 # Issue is with sb instruction, 
-soc.print_ram(0x100 / 4, 1)
 soc.load_data_to_ram("/tmp/timer.hex")
-soc.tick(50)
-# expected value at 0x100 address after the sequence is 0x55555555
-# real value at 0x100 address is 0x00000055
-soc.print_ram(0x100 / 4, 1)
+soc.tick(100)
 
