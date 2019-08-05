51dc437bfc58b3f32e6b4bd95f3e34bdffd380bd
ecco_the_dolphin <kupokupokupopo@gmail.com>
2019-08-05 11:47:50 +0300

    initial layout for minimalistic libc

---
+++ bench/tools/libc/include/stdlib.h
@@ -0,0 +1,7 @@
+#ifndef __INCLUDE_GUARD_RISCV_STDLIB__
+#define __INCLUDE_GUARD_RISCV_STDLIB__
+
+void exit(int __status) __attribute__ ((noreturn));
+
+#endif /* end of include guard: __INCLUDE_GUARD_RISCV_STDLIB__ */
+
