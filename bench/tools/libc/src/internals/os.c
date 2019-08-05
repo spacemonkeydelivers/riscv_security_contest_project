d8df513479d1ef4e8b43535367317255e0fbc642
ecco_the_dolphin <kupokupokupopo@gmail.com>
2019-08-05 15:37:19 +0300

    mostly complete minimalistic libc
    ISR does not have routing mechanism: in-progress

--- bench/tools/libc/src/internals/os.c
+++ bench/tools/libc/src/internals/os.c
@@ -3,10 +3,13 @@
 #include <stdint.h>
 
 
+__attribute__((section(".__system.data")))
 uint32_t __EXC_service[RISCV_EXC_TOTAL] = {};
+__attribute__((section(".__system.data")))
 uint32_t __INT_service[RISCV_INT_TOTAL] = {};
 
 
+__attribute__((section(".__system.os")))
 exc_handler_t register_exc_handler(int exc, exc_handler_t h) {
     exc_handler_t result = 0;
     if (exc < 0) {
@@ -21,6 +24,7 @@ exc_handler_t register_exc_handler(int exc, exc_handler_t h) {
     }
     return result;
 }
+__attribute__((section(".__system.os")))
 int_handler_t register_int_handler(int n, int_handler_t h) {
     int_handler_t result = 0;
     if (n < 0) {
@@ -36,6 +40,7 @@ int_handler_t register_int_handler(int n, int_handler_t h) {
     return result;
 }
 
+__attribute__((section(".__system.os")))
 int __exc_serve(struct s_esf_frame* frame, int exc) {
     if (exc < 0 || exc >= RISCV_EXC_TOTAL) {
         return -1;
@@ -47,6 +52,7 @@ int __exc_serve(struct s_esf_frame* frame, int exc) {
     return 0;
 }
 
+__attribute__((section(".__system.os")))
 int __int_serve(struct s_esf_frame* frame, int n) {
     if (n < 0 || n >= RISCV_INT_TOTAL) {
         return -1;
@@ -58,7 +64,7 @@ int __int_serve(struct s_esf_frame* frame, int n) {
     return 0;
 }
 
-
+__attribute__((section(".__system.init")))
 void __soc_init(const void* context)
 {
     (void)context;
