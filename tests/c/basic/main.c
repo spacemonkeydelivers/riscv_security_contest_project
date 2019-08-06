5ee9fdef4bca3651d78c2ab5b3c7124927bcaf91
ecco_the_dolphin <kupokupokupopo@gmail.com>
2019-08-06 10:05:42 +0300

    added a comment describing how to run a c-based tests on a simulator

--- tests/c/basic/main.c
+++ tests/c/basic/main.c
@@ -1,5 +1,15 @@
 #include <stdlib.h>
 
+
+/*
+    To run using OVPsim:
+    iss.exe --processorvendor riscv.ovpworld.org \
+            --processorname riscv \
+            --variant RV32IMC \
+            --program tests/c_basic/test.elf  \
+            --verbose -trace
+*/
+
 int main ()
 {
     exit(0);
