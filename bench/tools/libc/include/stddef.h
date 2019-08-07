c2d0dd19640e8d379a2fda3dac3e8b22b1247132
ecco_the_dolphin <kupokupokupopo@gmail.com>
2019-08-07 12:19:58 +0300

    initial printf integration

---
+++ bench/tools/libc/include/stddef.h
@@ -0,0 +1,11 @@
+#ifndef __INCLUDE_GUARD_RISCV_STDDEF__
+#define __INCLUDE_GUARD_RISCV_STDDEF__
+
+#include <stdint.h>
+
+typedef unsigned ptrdiff_t;
+typedef unsigned uintptr_t;
+
+#endif /* end of include guard: __INCLUDE_GUARD_RISCV_STDDEF__ */
+
+
