96245a7c9527b390563110555a74315785ff0154
ecco_the_dolphin <kupokupokupopo@gmail.com>
2019-08-13 08:59:32 +0300

    extend gen_enums to support new matching patterns

--- bench/tools/build/gen_enums.py
+++ bench/tools/build/gen_enums.py
@@ -1,6 +1,10 @@
 import sys
 import os
 
+EN_CLASS    = []
+PY_EN_WRAP  = []
+ASM_DEFINES = []
+
 def gen_enum(tgt_type, filename, matcher):
   matched_data = []
   input_file = os.path.join(sys.argv[1], filename)
@@ -13,36 +17,40 @@ def gen_enum(tgt_type, filename, matcher):
   if len(matched_data) == 0:
     raise Exception("no matches found")
 
-  print('// Autogenrated from <{}>'.format(input_file))
-  #matched_data.sort()
-  print('#ifndef D_GENERATE_SOC_ENUMS')
-  print('#ifndef D_SOC_HEADER_INCLUDE__GUARD')
-  print('#define D_SOC_HEADER_INCLUDE__GUARD')
-  print('enum class {} : int {{'.format(tgt_type))
+  EN_CLASS.append('// Autogenrated from <{}>'.format(input_file))
+  EN_CLASS.append('enum class {} : int {{'.format(tgt_type))
   for item in matched_data:
-    print('    {} = {}, '.format(item['en_item'], item['item_val']))
-  print('};')
-  print('#endif // D_SOC_HEADER_INCLUDE__GUARD')
-  print('#endif // D_GENERATE_SOC_ENUMS')
+    EN_CLASS.append('    {} = {}, '.format(item['en_item'], item['item_val']))
+  EN_CLASS.append('};')
 
-  print('')
-  print('')
-  print('#ifdef D_GENERATE_SOC_ENUMS')
   # print('BOOST_PYTHON_MODULE(enums) {')
-  print('enum_<{}>("{}")'.format(tgt_type, tgt_type))
+  PY_EN_WRAP.append('enum_<{}>("{}")'.format(tgt_type, tgt_type))
   for item in matched_data:
-    print('    .value("{}", {}::{})'.format(
+    PY_EN_WRAP.append('    .value("{}", {}::{})'.format(
       item['en_item'], tgt_type, item['en_item']))
-  print('    ;')
-  print('#endif //D_GENERATE_SOC_ENUMS')
+  PY_EN_WRAP.append('    ;')
+
+  ASM_DEFINES.append('// {} start'.format(tgt_type))
+  for item in matched_data:
+    ASM_DEFINES.append('#define  {}_{} {}'.format(
+        tgt_type.upper(), item['en_item'], item['item_val']))
+  ASM_DEFINES.append('// {} end\n'.format(tgt_type))
 
 
 import re
 
 def common_matcher(line, prefix):
-  m = re.match(r'\s+localparam\s+(\w+)\s*=\s*(\d+);', line)
+  m = re.match(r'\s*localparam\s+(\w+)\s*=\s*(\d+)\s*;', line)
+  t = 'dec'
   if m == None:
-    return None
+    m = re.match(r'\s*localparam\s+(\w+)\s*=\s*\d+\'b(\d+)\s*;', line)
+    t = 'bin'
+  if m == None:
+    m = re.match(r'\s*localparam\s+(\w+)\s*=\s*\d+\'h([0-9a-fA-F]+)\s*;', line)
+    t = 'hex'
+
+  if m == None:
+    return None;
 
   en_item = m.group(1)
   if not en_item.startswith(prefix):
@@ -50,9 +58,35 @@ def common_matcher(line, prefix):
 
   en_item = en_item[len(prefix):]
   item_val = m.group(2)
+
+  if t == 'bin':
+    item_val = int(item_val, 2)
+  elif t == 'hex':
+    item_val = int(item_val, 16)
+  else:
+    item_val = item_val
+
   return { 'en_item': en_item, 'item_val': item_val }
 
 gen_enum('en_state', 'rtl/cpu/cpu.v',
         lambda l: common_matcher(l, 'STATE_') )
 
+gen_enum('en_mcause', 'rtl/cpu/cpu.v',
+        lambda l: common_matcher(l, 'CAUSE_') )
 
+
+print('#ifndef D_SOC_HEADER_INCLUDE__GUARD')
+print('#define D_SOC_HEADER_INCLUDE__GUARD')
+print('#ifdef __cplusplus ')
+print('\n'.join(EN_CLASS))
+print('#endif // __cplusplus')
+
+print('#define __SOC_IS_NOT_SYSTEM_CONFORMANT')
+print('\n'.join(ASM_DEFINES))
+print('#endif // D_SOC_HEADER_INCLUDE__GUARD')
+
+print('')
+
+print('#ifdef D_GENERATE_SOC_ENUMS')
+print('\n'.join(PY_EN_WRAP))
+print('#endif // D_GENERATE_SOC_ENUMS')
