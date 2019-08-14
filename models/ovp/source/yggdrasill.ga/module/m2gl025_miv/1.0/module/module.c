c2299048965f5c7ab8495febad8b83da462e226a
RovingStone <matveev.d3v@yandex.ru>
2019-08-14 16:33:06 +0300

    Prepare some changes before VLNV build

---
+++ models/ovp/source/yggdrasill.ga/module/m2gl025_miv/1.0/module/module.c
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2005-2019 Imperas Software Ltd., www.imperas.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//                W R I T T E N   B Y   I M P E R A S   I G E N
+//
+//                             Version 20190628.0
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+#include <string.h>
+#include <stdlib.h>
+
+#include "op/op.h"
+
+#define MODULE_NAME "m2gl025_miv"
+
+typedef struct optModuleObjectS {
+    // insert module persistent data here
+} optModuleObject;
+
+
+// forward declaration of component constructor
+static OP_CONSTRUCT_FN(instantiateComponents);
+
+static OP_CONSTRUCT_FN(moduleConstructor) {
+
+    // instantiate module components
+    instantiateComponents(mi, object);
+
+    // insert constructor code here
+}
+
+static OP_PRE_SIMULATE_FN(modulePreSimulate) {
+    // insert pre simulation code here
+}
+
+static OP_SIMULATE_STARTING_FN(moduleSimulateStart) {
+    // insert simulation starting code here
+}
+
+static OP_POST_SIMULATE_FN(modulePostSimulate) {
+    // insert post simulation code here
+}
+
+static OP_DESTRUCT_FN(moduleDestruct) {
+    // insert destructor code here
+}
+
+#include "module.igen.h"
