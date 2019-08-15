9cee55fb42d0355a6c455eb89702493d13f359f2
ecco_the_dolphin <kupokupokupopo@gmail.com>
2019-08-15 15:33:53 +0300

    initial skeleton for debugger implementation

--- bench/rtl/python/benchlibs/debug.py
+++ bench/rtl/python/benchlibs/debug.py
@@ -9,11 +9,65 @@ from prompt_toolkit.history import FileHistory
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 
 
+class Print:
+    def __init__(self, benchlib, soc):
+        self._soc = soc
+        self._bench = benchlib
+
+    def help(self):
+        print("\n".join(['"print" command.',
+          'Synopsys: print [regs|cpu.state|pc]'
+          ]))
+
+    def run(self, args):
+        print('Print::run')
+        return None
+
+class Step:
+    def __init__(self, benchlib, soc):
+        self._soc = soc
+        self._bench = benchlib
+
+    def help(self):
+        print("\n".join(['"step" command.',
+          'Synopsys: step [num]'
+          ]))
+
+    def run(self, args):
+        print('Step::run')
+        return None
+
+class Help:
+    def __init__(self, benchlib, soc, debugger):
+        self._soc = soc
+        self._bench = benchlib
+
+    def help(self):
+        self.run([])
+
+    def run(self, args):
+        print("\n".join(['Help::run',
+           'we have the following command supported:',
+           '    - print',
+           '    - step',
+           '    - tick',
+           '    - e[X|x]amine',
+           '    - go',
+           'Please run `help <cmd_name>` for the usage details'
+           ]))
+        return None
+
 class Debugger:
     def __init__(self, benchlib, soc):
         self._disasm = disasm_lib.Disassembler(None)
         self._bench  = benchlib
         self._soc    = soc
+        self._cmd    = {}
+
+
+        self._cmd['print'] = Print(benchlib, soc)
+        self._cmd['step'] = Step(benchlib, soc)
+        self._cmd['help'] = Help(benchlib, soc, self)
 
         self._trace = {
             'state': None,
@@ -63,9 +117,29 @@ class Debugger:
 
     def repl(self):
         while True:
-            user_input = prompt('>',
-                history=FileHistory('.history.txt'),
-                auto_suggest=AutoSuggestFromHistory(),
-            )
-            print(user_input)
+            try:
+                user_input = prompt('>',
+                    history=FileHistory('.history.txt'),
+                    auto_suggest=AutoSuggestFromHistory(),
+                )
+            except EOFError:
+                break
+
+            input_cmd = [x.strip() for x in user_input.split()]
+            input_cmd = [i for i in input_cmd if i] 
+
+            if len(input_cmd) == 0:
+                continue
+
+            cmd = input_cmd[0]
+            input_cmd.pop(0)
+
+            if self._cmd.has_key(cmd):
+                if len(input_cmd) > 0:
+                  if input_cmd[0] == 'help':
+                        self._cmd[cmd].help()
+                        continue
+                self._cmd[cmd].run(input_cmd)
+            else:
+                print("Error: unknown command {}".format(cmd))
 
