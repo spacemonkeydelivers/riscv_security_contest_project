cd814384de1a727d7c2d67f79f6a3337be1d2b26
ecco_the_dolphin <kupokupokupopo@gmail.com>
2019-08-16 10:47:38 +0300

    implemented `tick` debugger command

--- bench/rtl/python/benchlibs/debugger/cmd_tick.py
+++ bench/rtl/python/benchlibs/debugger/cmd_tick.py
@@ -1,18 +1,39 @@
 class CmdTick:
     def __init__(self, benchlib, soc, debugger):
-        self._soc = soc
-        self._bench = benchlib
+        self.soc = soc
+        self.bench = benchlib
+        self.debugger = debugger
 
     def names(self):
         return ['tick']
 
     def help(self):
         print("\n".join(['"tick" command',
-          'Synopsis: tick [TODO]',
+          'Synopsis: tick [number]',
+          'Simulate single "tick" (clk low->clk high transitions).',
+          'May be useful for debugging of issues during execution of a single instruction.'
           ]))
 
     def run(self, args):
-        print('Tick::run')
+        if len(args) == 0:
+            args = [1]
+
+        if len(args) == 1:
+            num_str = args[0]
+            try:
+                num = int(num_str)
+                print('--- Tick Start ---')
+                print('utrace before:')
+                self.debugger.print_utrace()
+                for i in range(0, num):
+                    self.soc.tick()
+                print('utrace after:')
+                self.debugger.print_utrace()
+                print('__________________')
+            except ValueError:
+                print 'Error: could not figure out how many ticks required'
+            return
+  
         print('Error: incorrect <tick> command')
         return None
 
