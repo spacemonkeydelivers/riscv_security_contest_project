c1c1fe0d7d616b0ea3ae6d74f4d29c853e59aaec
ecco_the_dolphin <kupokupokupopo@gmail.com>
2019-08-16 21:41:43 +0300

    fixed a bug in isr IALIGN tests

--- tests/c/libc/isr_route_ialign_base/isr_route_ialign_base.c
+++ tests/c/libc/isr_route_ialign_base/isr_route_ialign_base.c
@@ -9,7 +9,7 @@ void ialign_handler(int n, void* context)
 }
 
 int main () {
-    register_int_handler(RISCV_EXC_I_ALIGN, &ialign_handler);
+    register_exc_handler(RISCV_EXC_I_ALIGN, &ialign_handler);
     __asm__ volatile ("li ra, 2\n"
                       "ret"
                       :
