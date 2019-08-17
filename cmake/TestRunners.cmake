ea9d1f1562454cd274d6d4eef922113b71d772b0
ecco_the_dolphin <kupokupokupopo@gmail.com>
2019-08-17 12:49:04 +0300

    facilites to label failed tests as "nigthly", add all passing tests to CI

--- cmake/TestRunners.cmake
+++ cmake/TestRunners.cmake
@@ -11,12 +11,20 @@ set(DEBUGGER_TEST_RUNNER ${CMAKE_SOURCE_DIR}/tests/debugger/run.sh)
 
 function(test_add name)
 
+    set(options NIGHTLY)
+    cmake_parse_arguments(PARSE_ARGV 1 TEST_DESCR "${options}" "" "")
+
     set(TEST_DIR "${CMAKE_BINARY_DIR}/tests/${name}")
     # This is a hack. Stupid cmake won't create working directories if not exist
     file(MAKE_DIRECTORY "${TEST_DIR}")
     add_test(NAME "${name}"
-             COMMAND "${TEST_RUNNER}" ${ARGN}
+             COMMAND "${TEST_RUNNER}" ${TEST_DESCR_UNPARSED_ARGUMENTS}
              WORKING_DIRECTORY "${TEST_DIR}")
+
+    if (${TEST_DESCR_NIGHTLY})
+        set_tests_properties("${name}" PROPERTIES LABELS nightly)
+    endif()
+
 endfunction()
 
 function(add_debugger_test name)
