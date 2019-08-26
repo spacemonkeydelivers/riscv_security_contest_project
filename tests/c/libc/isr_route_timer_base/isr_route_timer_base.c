10c6aa7ad1048ee78e6571443abea93bfcfba623
ecco_the_dolphin <kupokupokupopo@gmail.com>
2019-08-27 00:00:02 +0300

    tests: isr_route_timer test is hardened

--- tests/c/libc/isr_route_timer_base/isr_route_timer_base.c
+++ tests/c/libc/isr_route_timer_base/isr_route_timer_base.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include <soc/traps.h>
 #include <soc/timer.h>
@@ -6,27 +7,30 @@
 volatile int global = 0;
 volatile int unused = 0;
 
+#define D_TIMEOUT 30
 void timer_handler(int n, void* context)
 {
-    global = 1;
+    ++global;
 
-    alarm_soc_timer(0);
+    if (global < 100) {
+        alarm_soc_timer(D_TIMEOUT);
+    } else {
+        alarm_soc_timer(0);
+    }
 }
 
 int main () {
-    global = 0;
-    unused = 0;
     register_int_handler(RISCV_INT_EXT_M, &timer_handler);
 
-    if (!alarm_soc_timer(30)) {
+    if (!alarm_soc_timer(D_TIMEOUT)) {
         return EXIT_FAILURE;
     }
 
-    while (global == 0)
+    while (global < 100)
     {
         ++unused;
     }
-    exit(0);
+    printf("unused value: %d\n", unused);
     return EXIT_SUCCESS;
 }
 
