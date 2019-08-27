284a8f5f00867be6219e9cb0727fe05faa4eea00
Petr Kurapov <petr.kurapov@gmail.com>
2019-08-27 22:32:20 +0300

    [rtl] Fix M-extension complince tests behavior

--- tests/asm/mext/div.s
+++ tests/asm/mext/div.s
@@ -16,6 +16,22 @@ main:
     div t1, a0, a6
     li t2, 0xFFFFFFFE
     bne t1, t2, failed
+
+    li a0, 0x1
+    li a1, -0x1
+    li a2, 0xFFFFFFFF
+
+    div t1, a0, a2
+    li t2, 0xFFFFFFFF
+    bne t2, t1, failed
+    //li t3,
+    
+    //li t2, 0xFFFFFFFF
+    //bne t2, a1, failed
+    /*div t1, a0, a1
+    li t2, 0xFFFFFFFF
+    bne t1, t2, failed*/
+
 PASSED
 
 failed:
