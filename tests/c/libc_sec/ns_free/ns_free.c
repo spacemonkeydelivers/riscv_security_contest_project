9f47cd1c467cc4d12fc2f49fdf63d38d432c7983
ecco_the_dolphin <kupokupokupopo@gmail.com>
2019-08-28 11:54:13 +0300

    tests: additional security tests

---
+++ tests/c/libc_sec/ns_free/ns_free.c
@@ -0,0 +1,23 @@
+#include <soc/traps.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void secure_monitor(int n, void* context)
+{
+    if (n == RISCV_INT_SM_PANIC) {
+        printf("security panic detected! Test Failure!\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+/* SECURITY_CTRL: DISABLE */
+int main () {
+
+    register_int_handler(RISCV_INT_SM_PANIC, &secure_monitor);
+
+    unsigned char* ptr = (unsigned char*)malloc(64);
+    free(ptr);
+    ptr[0] = 0xff;
+    return EXIT_SUCCESS;
+}
