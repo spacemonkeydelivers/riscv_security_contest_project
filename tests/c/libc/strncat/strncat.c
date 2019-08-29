ceee042e2a7fceb67cfe82e304974f81b986b3dc
ecco_the_dolphin <kupokupokupopo@gmail.com>
2019-08-29 10:59:04 +0300

    tests: strncat/strncpy

--- tests/c/libc/strncat/strncat.c
+++ tests/c/libc/strncat/strncat.c
@@ -1,6 +1,142 @@
+#include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
 
+/*
+Appends at most count characters from the character array pointed to by src,
+stopping if the null character is found, to the end of the null-terminated byte
+string pointed to by dest. The character src[0] replaces the null terminator at
+the end of dest. The terminating null character is always appended in the end
+(so the maximum number of bytes the function may write is count+1).  The
+behavior is undefined if the destination array does not have enough space for
+the contents of both dest and the first count characters of src, plus the
+terminating null character. The behavior is undefined if the source and
+destination objects overlap. The behavior is undefined if either dest is not a
+pointer to a null-terminated byte string or src is not a pointer to a character
+array
+*/
+bool case1 () {
+    char buffer[10];
+    char ref_buff[10];
+    memset(buffer, 1, sizeof(buffer));
+    buffer[0] = 0;
+    memset(ref_buff, 1, sizeof(ref_buff));
+    char* result = strncat(buffer, "", 0);
+    if (buffer[0] != 0) {
+        printf("case1: no terminating null after strncat\n");
+        return false;
+    }
+    if (result != buffer) {
+        printf("case1: returned value does not match the expected\n");
+        return false;
+    }
+    if (memcmp(buffer + 1, ref_buff, sizeof(buffer) - 1) != 0) {
+        printf("case1: destination is not consistent\n");
+        return false;
+    }
+    return true;
+}
+bool case2 () {
+    char buffer[10];
+    char ref_buff[10];
+    memset(buffer, 1, sizeof(buffer));
+    buffer[0] = 0;
+    memset(ref_buff, 1, sizeof(ref_buff));
+    char* result = strncat(buffer, "", 1);
+    if (buffer[0] != 0) {
+        printf("case2: no terminating null after strncat\n");
+        return false;
+    }
+    if (result != buffer) {
+        printf("case2: returned value does not match the expected\n");
+        return false;
+    }
+    if (memcmp(buffer + 1, ref_buff, sizeof(buffer) - 1) != 0) {
+        printf("case2: destination is not consistent\n");
+        return false;
+    }
+    return true;
+}
+bool case3 () {
+    char buffer[10];
+    memset(buffer, 1, sizeof(buffer));
+    buffer[0] = 'H';
+    buffer[1] = 'e';
+    buffer[2] = 0;
+    char* result = strncat(buffer, "llo", 2);
+    if (result != buffer) {
+        printf("case3: returned value does not match the expected\n");
+        return false;
+    }
+    if (buffer[5] != 0) {
+        printf("case3: no terminating character detected at pos #5\n");
+        return false;
+    }
+    if (memcmp(buffer, "Hell", 4) != 0) {
+        printf("case3: the data in the destination is incorrect\n");
+        return false;
+    }
+    return true;
+}
+bool case4 () {
+    char buffer[10];
+    memset(buffer, 1, sizeof(buffer));
+    buffer[0] = 'H';
+    buffer[1] = 'e';
+    buffer[2] = 0;
+    char* result = strncat(buffer, "llo", 3);
+    if (buffer[6] != 0) {
+        printf("case4: no terminating character detected at pos #6\n");
+        return false;
+    }
+    if (result != buffer) {
+        printf("case4: returned value does not match the expected\n");
+        return false;
+    }
+    if (memcmp(buffer, "Hello", 5) != 0) {
+        printf("case4: the data in the destination is incorrect\n");
+        return false;
+    }
+    return true;
+}
+bool case5 () {
+    char buffer[10];
+    memset(buffer, 1, sizeof(buffer));
+    buffer[0] = 'H';
+    buffer[1] = 'e';
+    buffer[2] = 0;
+    char* result = strncat(buffer, "llo", 100500);
+    if (buffer[6] != 0) {
+        printf("case5: no terminating character detected at pos #6\n");
+        return false;
+    }
+    if (result != buffer) {
+        printf("case5: returned value does not match the expected\n");
+        return false;
+    }
+    if (memcmp(buffer, "Hello", 5) != 0) {
+        printf("case5: the data in the destination is incorrect\n");
+        return false;
+    }
+    return true;
+}
+/*bool case6 () {*/
+    /*char buffer[10];*/
+    /*char ref_buff[10];*/
+/*}*/
 int main () {
-    int result = EXIT_FAILURE;
-    return result;
+    // case_1:
+    if (!case1()) { return EXIT_FAILURE; }
+    // case_2
+    if (!case2()) { return EXIT_FAILURE; }
+    // case_3
+    if (!case3()) { return EXIT_FAILURE; }
+    // case_4
+    if (!case4()) { return EXIT_FAILURE; }
+    // case_5
+    if (!case5()) { return EXIT_FAILURE; }
+    // case_6
+    // if (!case6()) { return EXIT_FAILURE; }
+
+    return EXIT_SUCCESS;
 }
--- tests/c/libc/strncpy/strncpy.c
+++ tests/c/libc/strncpy/strncpy.c
@@ -1,6 +1,139 @@
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
 
+/*
+strncpy
+Copies at most count characters of the byte string pointed to by src (including
+the terminating null character) to character array pointed to by dest.
+
+If count is reached before the entire string src was copied, the resulting
+character array is not null-terminated.
+
+If, after copying the terminating null character from src, count is not
+reached, additional null characters are written to dest until the total of
+count characters have been written.
+ */
+bool case1 () {
+    char buff1[10];
+    memset(buff1, 0xff, sizeof(buff1));
+    char* sr1 = strncpy(buff1, "123456789", 9);
+    if (memcmp(buff1, "123456789", 9) != 0) {
+        printf("case1: *strncpy* could not copy the data (data is corrupted)\n");
+        return false;
+    }
+    if (sr1 != buff1) {
+        printf("case1: the return value does not match the expected one\n");
+        return false;
+    }
+    if (buff1[9] != 0xff) {
+        printf("case1: the terminating 0 shoud not be copied if count does not allow\n");
+        return false;
+    }
+    return true;
+}
+bool case2 () {
+    char buff1[10];
+    memset(buff1, 0xaa, sizeof(buff1));
+    char* sr2 = strncpy(buff1, "", 2);
+    if (sr2 != buff1) {
+        printf("case2: the return value does not match the expected one\n");
+        return false;
+    }
+    if (buff1[0] != 0) {
+        printf("case2: terminating was not copied\n");
+        return false;
+    }
+    if (buff1[1] != 0) {
+        printf("case2: insufficient number of bytes were copied\n");
+        return false;
+    }
+    if (buff1[2] != 0xaa) {
+        printf("case2: too much data was copied\n");
+        return false;
+    }
+    return true;
+}
+bool case3 () {
+    char buff1[10];
+    memset(buff1, 0xbb, sizeof(buff1));
+    char* sr3 = strncpy(buff1, "12", 0);
+    if (buff1[0] != 0xbb) {
+        printf("case3: no data should be copied if count is 0\n");
+        return false;
+    }
+    if (sr3 != buff1) {
+        printf("case3: the return value does not match the expected one\n");
+        return false;
+    }
+    return true;
+}
+bool case4 () {
+    char buff1[10];
+    memset(buff1, 0xba, sizeof(buff1));
+    char* sr4 = strncpy(buff1, "12", 3);
+    if (buff1[3] != 0) {
+        printf("case4: no terminating character detected at pos #3\n");
+        return false;
+    }
+    if ((buff1[0] != '1') || (buff1[1] != '2')) {
+        printf("case4: desination is corrupted\n");
+        return false;
+    }
+    if (sr4 != buff1) {
+        printf("case4: the return value does not match the expected one\n");
+        return false;
+    }
+    return true;
+}
+bool case5 () {
+    char buff1[10];
+    memset(buff1, 0xba, sizeof(buff1));
+    char* sr5 = strncpy(buff1, "12", 2);
+    if (buff1[3] == 0) {
+        printf("case5: terminating character detected at pos #3\n");
+        return false;
+    }
+    if (sr5 != buff1) {
+        printf("case5: the return value does not match the expected one\n");
+        return false;
+    }
+    if ((buff1[0] != '1') || (buff1[1] != '2')) {
+        printf("case5: desination is corrupted\n");
+        return false;
+    }
+    return true;
+}
+bool case6 () {
+    char buff1[10];
+    memset(buff1, 0xba, sizeof(buff1));
+    char* sr6 = strncpy(buff1, "12", 1);
+    if (sr6 != buff1) {
+        printf("case6: the return value does not match the expected one\n");
+        return false;
+    }
+    if (buff1[0] != '1') {
+        printf("case6: desination is corrupted\n");
+        return false;
+    }
+    if (buff1[1] == 0) {
+        printf("case6: terminating character detected at pos #1\n");
+        return false;
+    }
+    return true;
+}
 int main () {
-    int result = EXIT_FAILURE;
-    return result;
+    // case_1:
+    if (!case1()) { return EXIT_FAILURE; }
+    // case_2
+    if (!case2()) { return EXIT_FAILURE; }
+    // case_3
+    if (!case3()) { return EXIT_FAILURE; }
+    // case_4
+    if (!case4()) { return EXIT_FAILURE; }
+    // case_5
+    if (!case5()) { return EXIT_FAILURE; }
+    // case_6
+    if (!case6()) { return EXIT_FAILURE; }
+    return EXIT_SUCCESS;
 }
