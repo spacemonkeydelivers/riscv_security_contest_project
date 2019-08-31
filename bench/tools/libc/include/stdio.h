988cdebb025a4d2c4e00ae181cf05ec5e6eaa89a
Igor Chervatyuk <outofhere@mail.ru>
2019-08-31 10:37:29 +0300

    Modifications in order to run riscv-rippe-framework. Part of changes provided by F.Veselovsky

--- bench/tools/libc/include/stdio.h
+++ bench/tools/libc/include/stdio.h
@@ -6,12 +6,16 @@
 
 #include <misc/toolchain.h>
 
+#define stdin 0
+#define stdout 1
+#define stderr 2
+
 extern int __printf_like(1, 2) printf_(const char* format, ...);
 int __printf_like(2, 3) sprintf_(char* buffer, const char* format, ...);
 int __printf_like(3, 4) snprintf_(char* buffer, size_t count, const char* format, ...);
 int __printf_like(3, 0) vsnprintf_(char* buffer, size_t count, const char* format, va_list va);
 int __printf_like(1, 0) vprintf_(const char* format, va_list va);
-
+int fprintf_ (int stream, const char * format, ...);
 // use output function (instead of buffer) for streamlike interface
 int fctprintf(void (*out)(char character, void* arg), void* arg, const char* format, ...);
 
@@ -20,6 +24,7 @@ int fctprintf(void (*out)(char character, void* arg), void* arg, const char* for
 #define snprintf  snprintf_
 #define vsnprintf vsnprintf_
 #define vprintf vprintf_
+#define fprintf fprintf_
 
 #endif /* end of include guard: __INCLUDE_GUARD_RISCV_STDIO__ */
 
