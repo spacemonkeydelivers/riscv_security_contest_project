b78a9c62fb20edd12073f0428134fd69a53d8fb4
ecco_the_dolphin <kupokupokupopo@gmail.com>
2019-09-01 20:01:01 +0300

    extend simple example by showing how to call debugger commands directly

--- tests/asm/examples/simple.s.py
+++ tests/asm/examples/simple.s.py
@@ -3,6 +3,12 @@ import benchlibs.debug as debug
 def run(libbench, soc):
   dbg = debug.Debugger(libbench, soc)
   dbg.set_tracing_enabled(True)
+
+  dbg.process_input("x /i4 0")
+
   dbg.stepi(4)
   dbg._soc.tick()
 
+  dbg.process_input("p")
+  print("")
+  dbg.process_input("x /x4 0")
