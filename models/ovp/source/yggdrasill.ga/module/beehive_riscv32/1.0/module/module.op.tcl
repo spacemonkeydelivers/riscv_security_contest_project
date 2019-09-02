22ee9633aac80d05a8943d74834a3d7fe725a85a
RovingStone <matveev.d3v@yandex.ru>
2019-09-02 19:43:00 +0300

    [ovp] beehive_riscv32 board with BeehiveUART

---
+++ models/ovp/source/yggdrasill.ga/module/beehive_riscv32/1.0/module/module.op.tcl
@@ -0,0 +1,77 @@
+# Binary Translator Bedouins were here
+#
+# yggdrasill.ga
+#
+
+#### PLATFORM
+set vendor yggdrasill.ga
+set library module
+set name beehive_riscv32
+set version 1.0
+
+ihwnew -name $name -vendor $vendor -library $library -version $version -purpose module
+
+iadddocumentation -name Licensing   -text "No license"
+iadddocumentation -name Description -text "The operating system Paradise"
+
+#### Memory map
+# sram0@00000000 reg = <0x0 0x00000000 0x0 0x40000000>;
+# timer@40000000 reg = <0x0 0x40000000 0x0 0x00000018>;
+# uart0@80000000 reg = <0x0 0x80000000 0x0 0x40000000>;
+
+#### PROCESSOR
+ihwaddprocessor \
+  -instancename hart0 \
+  -vendor microsemi.ovpworld.org \
+  -library processor \
+  -type riscv \
+  -version 1.0 \
+  -variant MiV_RV32IMA
+
+ihwaddbus \
+  -instancename bus0 -addresswidth 32
+
+ihwconnect \
+  -bus bus0 -instancename hart0 -busmasterport INSTRUCTION
+
+ihwconnect \
+  -bus bus0 -instancename hart0 -busmasterport DATA
+
+#### MEMORY
+# SRAM0
+set sram0_lo 0x00000000
+set sram0_sz 0x40000000
+set sram0_hi [expr $sram0_lo + $sram0_sz - 1]
+
+ihwaddmemory \
+  -instancename sram0 -type ram
+
+ihwconnect \
+  -bus bus0 -instancename sram0 -busslaveport sp1 \
+  -loaddress $sram0_lo -hiaddress $sram0_hi
+
+#### PERIPHERAL
+# UART0
+set uart0_lo 0x80000000
+set uart0_sz       0x18
+set uart0_hi [expr $uart0_lo + $uart0_sz - 1]
+
+ihwaddperipheral \
+  -instancename uart0 -type BeehiveUART \
+  -vendor yggdrasill.ga -library peripheral -version 1.0
+
+ihwconnect \
+  -instancename uart0 -busslaveport port0 -bus bus0 \
+  -loaddress $uart0_lo -hiaddress $uart0_hi
+
+# TIMER STUB
+set timer0_lo 0x40000000
+set timer0_sz 0x40000000
+set timer0_hi [expr $timer0_lo + $timer0_sz - 1]
+
+ihwaddmemory \
+  -instancename timer0 -type ram
+
+ihwconnect \
+  -bus bus0 -instancename timer0 -busslaveport sp1 \
+  -loaddress $timer0_lo -hiaddress $timer0_hi
