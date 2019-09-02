57e9559a3487690fb67ae229f2f46e6b6f3753c7
brown25 <asetium@gmail.com>
2019-09-03 01:00:02 +0300

    rtl: lfsr_rnd based on polynomial taken from XILINX application note

--- rtl/cpu/lfsr_rnd.v
+++ rtl/cpu/lfsr_rnd.v
@@ -1,33 +1,31 @@
 module lfsr_rnd
-    #(parameter BITS = 31)
+    #(parameter BITS = 32)
     (
-     input        I_clk,
-     input        I_reset,
-     output[31:0] O_rnd
+        input        I_clk,
+        input        I_reset,
+        output[31:0] O_rnd
     );
 
     wire clk, reset;
     assign clk = I_clk;
     assign reset = I_reset;
 
-	reg[31:0] result;
-	assign O_rnd = result;
+    reg[31:0] result;
+    assign O_rnd = result;
 
-    reg [31:0] next;
-    always @(*) begin
-       next = result;
-       repeat(BITS) begin
-          next = {(next[31]^next[1]), next[31:1]};
-       end
-    end
-
+    integer i;
+    localparam POLY = 32'h80200003;
     always @(posedge clk) begin
-       if(I_reset) begin
-          result <= 32'hbed4dead;
-       end
-       else begin
-          result <= next;
-       end
+        if(I_reset) begin
+            result <= 32'hbed4dead;
+        end
+        else begin
+            result[0] <= result[31];
+            for (i = 1; i < BITS; i = i + 1) begin
+                if (POLY & (1 << i)) result[i] <= result[i - 1] ^ result[31];
+                else result[i] <= result[i - 1];
+            end
+        end
     end
 
 endmodule
