b0183c14cc6a973254771430a5ab23b9e975ae53
RovingStone <matveev.d3v@yandex.ru>
2019-09-12 20:17:39 +0300

    tests: asm timer test (with MIP and MIE checks)

--- tests/asm/timer.s
+++ tests/asm/timer.s
@@ -3,31 +3,61 @@
 
 .text
 main:
-mv sp, zero
-lui sp, 0x40000
-// count clocks
-addi a0, zero, 1
-sw a0, 16(sp)
-// up to 10 clocks
-addi a0, zero, 5
-sb a0, 8(sp)
-mv zero, zero
-mv zero, zero
-mv zero, zero
-mv zero, zero
-mv zero, zero
-mv zero, zero
-lb a0, 0(sp)
-sb zero, 1(sp)
-mv zero, zero
-mv zero, zero
-mv zero, zero
+    mv sp, zero
+    lui sp, 0x40000
 
-FAILED 1
+    // Disable interrupts
+    li t2, 0
+    csrw mie, t2
+
+    // Tick on every clock
+    addi a0, zero, 1
+    sw a0, 16(sp)
+
+    // up to 1 clock, should interrupt
+    // immediately after MIE wirte
+    addi a0, zero, 1
+    sb a0, 8(sp)
+
+    // Enable interrupts
+    li t2, 0x80
+    csrw mie, t2
+
+    mv zero, zero
+    mv zero, zero
+    mv zero, zero
+    mv zero, zero
+    mv zero, zero
+    mv zero, zero
+    lb a0, 0(sp)
+    sb zero, 1(sp)
+    mv zero, zero
+    mv zero, zero
+    mv zero, zero
+
+    FAILED 1
 
 ON_EXCEPTION:
-mv zero, zero
-mv zero, zero
-mv zero, zero
-sb a0, 8(sp)
+    nop
+
+check_mip_bit_set:
+    csrr t0, mip
+    andi t0, t0, 0x80
+    bnez t0, check_mip_mtimecmp
+    FAILED 2
+
+check_mip_mtimecmp:
+    mv sp, zero
+    lui sp, 0x40000
+    li t0, -1
+    sw t0, 12(sp)
+
+    // check MIP is zero
+    csrr t0, mip
+    andi t0, t0, 0x80
+    beqz t0, looks_good
+    FAILED 3
+
+looks_good:
+    sb a0, 8(sp)
     PASSED
