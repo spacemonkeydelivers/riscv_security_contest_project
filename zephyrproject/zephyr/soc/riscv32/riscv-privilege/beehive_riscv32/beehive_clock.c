e6bb6602b645fb39ad8345ea2748cedd7871efd1
Sergei Matveev <matveev.d3v@yandex.ru>
2019-09-13 20:44:06 +0300

    zephyr: timer freq and hw clock speed tuning

--- zephyrproject/zephyr/soc/riscv32/riscv-privilege/beehive_riscv32/beehive_clock.c
+++ zephyrproject/zephyr/soc/riscv32/riscv-privilege/beehive_riscv32/beehive_clock.c
@@ -2,12 +2,12 @@
 
 #include "soc.h"
 
-static int beehive_clock_init(struct device *dev) 
+static int beehive_clock_init(struct device *dev)
 {
     ARG_UNUSED(dev);
 
     volatile u32_t *r = (u32_t *)BEEHIVE_MTIMECTRL_BASE;
-    u64_t freq = 4;  // to lower freq
+    u64_t freq = 1;  // to lower freq
 
     r[1] = (u32_t)(freq >> 32);
     r[0] = (u32_t)freq;
