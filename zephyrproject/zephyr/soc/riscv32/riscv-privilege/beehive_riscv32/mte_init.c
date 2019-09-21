ea15169acd94052e082a51822ce00b0215880cf9
ecco_the_dolphin <kupokupokupopo@gmail.com>
2019-09-21 15:32:48 +0300

    [latechange] zephyr: support for soc-specific SecureMonitorPanic interrupt

--- zephyrproject/zephyr/soc/riscv32/riscv-privilege/beehive_riscv32/mte_init.c
+++ zephyrproject/zephyr/soc/riscv32/riscv-privilege/beehive_riscv32/mte_init.c
@@ -2,9 +2,26 @@
 
 #include "soc.h"
 
+static void secure_monitor(void* arg)
+{
+    ARG_UNUSED(arg);
+    printk("\n------\nSecure Monitor Panic detected!\n------\n");
+    __asm__ __volatile__(
+            "li ra, 0xdead\n\t"
+            "1:\n\t"
+            "j 1b\n\t"
+            "wfi\n\t"
+            : /* No outputs. */
+            :
+            : "memory");
+
+}
 static int beehive_mte_init(struct device *dev)
 {
     ARG_UNUSED(dev);
+
+    IRQ_CONNECT(RISCV_BEEHIVE_SECURE_MONITOR_PANIC_IRQ, 0, secure_monitor, NULL, 0);
+    irq_enable(RISCV_BEEHIVE_SECURE_MONITOR_PANIC_IRQ);
 
     int en_value = 1;
     __asm__ __volatile__(
