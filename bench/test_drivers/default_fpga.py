481ce855a4f63bb838297939e4f9ee2a347ef50e
ecco_the_dolphin <kupokupokupopo@gmail.com>
2019-09-25 09:51:34 +0300

    more refactoring on remote runners

---
+++ bench/test_drivers/default_fpga.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python2
+
+import sys
+import os
+import subprocess
+import glob
+import imp
+import re
+import time
+
+from dut_wrapper.fpga import FPGA_SOC
+#TODO: rename to libdut
+import libbench
+
+def main(filename):
+    if not os.path.isfile(filename):
+        print 'could not find input file <{}>'.format(filename)
+        raise RuntimeError('incorrect input file')
+
+    #TODO: rename to libdut
+    soc = FPGA_SOC(libbench, "/dev/fpga")
+
+    print("...fpga_init")
+    soc.fpga_init()
+    print("...check_sanity")
+    soc.check_sanity()
+    print("...halt_soc")
+    soc.halt_soc()
+
+    print("...set_baud")
+    soc.uart_set_baud_9600()
+
+    soc.uart_print("\nNew testing sequence initiated!\n")
+    soc.uart_print("Uploading memory image...\n")
+    soc.upload_image(filename)
+    soc.uart_print("Memory image uploaded, initiating test run\n")
+    soc.uart_print("----->\n")
+
+    soc.run_soc()
+    time.sleep(3)
+
+    soc.print_cpu_pc()
+    soc.print_cpu_state()
+
+    soc.halt_soc()
+
+    print(hex(soc.get_cpu_pc()))
+    print(soc.get_cpu_state())
+
+    print("#######################")
+    #soc.upload_image("/mnt/smd/fpga_tests/libc_printk.v")
+
+    #soc.write_word(0x0,  0x10000113)
+    #soc.write_word(0x4,  0x12300513)
+    #soc.write_word(0x8,  0x20000593)
+    #soc.write_word(0xc,  0x00a12023)
+    #soc.write_word(0x10, 0x00b50633)
+    #soc.write_word(0x14, 0x00c12223)
+    #soc.write_word(0x18, 0x0a11c0b7)
+    #soc.write_word(0x1c, 0x00108093)
+    #soc.write_word(0x20, 0x0000006f)
+    #soc.print_ram(0, 40)
+    # soc.print_ram(0x100, 2)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
