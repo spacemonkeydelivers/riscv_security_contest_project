9b1bdad132b999750a2efab4f94a903a7881192c
ecco_the_dolphin <kupokupokupopo@gmail.com>
2019-11-18 09:13:17 +0300

    fix coremark compilation

--- tests/benchmarks/coremark/core_portme.c
+++ tests/benchmarks/coremark/core_portme.c
@@ -109,6 +109,7 @@ ee_u32 default_num_contexts=1;
 */
 void portable_init(core_portable *p, int *argc, char *argv[])
 {
+  (void)argc;(void)argv;
 	if (sizeof(ee_ptr_int) != sizeof(ee_u8 *)) {
 		ee_printf("ERROR! Please define ee_ptr_int to a type that holds a pointer!\n");
 	}
