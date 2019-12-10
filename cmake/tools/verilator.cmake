3cb9cadb01907bc967f896700c9ec7981bbe6dec
Alex White <space.monkey.delivers@gmail.com>
2019-12-10 21:32:07 +0300

    Use fastest compiler flags

--- cmake/tools/verilator.cmake
+++ cmake/tools/verilator.cmake
@@ -14,6 +14,6 @@ set(VERILATOR_FLAGS "")
 if(ENABLE_VCD_TRACING)
     set(VERILATOR_FLAGS "${VERILATOR_FLAGS} --trace --trace-params --trace-structs")
 endif()
-set(VERILATOR_FLAGS "${VERILATOR_FLAGS} -Wall --MMD --public -O3")
-set(VERILATOR_FLAGS "${VERILATOR_FLAGS} -CFLAGS -fpic")
+set(VERILATOR_FLAGS "${VERILATOR_FLAGS} --MMD -O3 -Wall -Wno-UNUSED")
+set(VERILATOR_FLAGS "${VERILATOR_FLAGS} -CFLAGS -fpic -CFLAGS -O3 -CFLAGS -fno-stack-protector -CFLAGS -fstrict-aliasing -CFLAGS -fbranch-probabilities -CFLAGS -flto -LDFLAGS -flto")
 
