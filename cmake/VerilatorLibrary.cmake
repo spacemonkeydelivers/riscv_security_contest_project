978690ff040b89b97710d4e29f505b1afc067f71
ecco_the_dolphin <kupokupokupopo@gmail.com>
2019-12-10 23:46:03 +0300

    [bugfix] fix multithreaded builds of verilated models
    To make multi-threaded build work we pass -DVL_THREADED to libverilated. We should
    also pick up verilated_threads.cpp to access ThreadPool implementation

--- cmake/VerilatorLibrary.cmake
+++ cmake/VerilatorLibrary.cmake
@@ -5,15 +5,24 @@ set(VERILATED_SOURCE_LIST
     verilated_save.cpp
     verilated_dpi.cpp
 )
+
+if(ENABLE_THREADING)
+  set(VERILATED_SOURCE_LIST ${VERILATED_SOURCE_LIST} verilated_threads.cpp)
+endif()
 if (VERILATOR_FLAGS MATCHES "trace")
-    set(VERILATED_SOURCE_LIST ${VERILATED_SOURCE_LIST} verilated_vcd_c.cpp)
+  set(VERILATED_SOURCE_LIST ${VERILATED_SOURCE_LIST} verilated_vcd_c.cpp)
 endif()
 
 foreach(src ${VERILATED_SOURCE_LIST})
-    set(VERILATED_SOURCES ${VERILATED_SOURCES} ${VERILATOR_INCLUDE}/${src})
+  set(VERILATED_SOURCES ${VERILATED_SOURCES} ${VERILATOR_INCLUDE}/${src})
 endforeach()
 
 add_library(${VERILATOR_LIBRARY} SHARED ${VERILATED_SOURCES})
+
+if(ENABLE_THREADING)
+  target_compile_options(${VERILATOR_LIBRARY} PUBLIC -DVL_THREADED)
+endif()
+
 target_include_directories(${VERILATOR_LIBRARY} SYSTEM PUBLIC ${VERILATOR_INCLUDE})
 target_include_directories(${VERILATOR_LIBRARY} SYSTEM PUBLIC ${VERILATOR_INCLUDE}/vltstd/)
 
