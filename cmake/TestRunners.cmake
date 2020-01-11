fb55b328dfe7bc8d868d4f179affe49a4517f1b0
ecco_the_dolphin <kupokupokupopo@gmail.com>
2020-01-12 01:34:25 +0300

    mte-enabling for c-based tests should be conducted directly in test lists

--- cmake/TestRunners.cmake
+++ cmake/TestRunners.cmake
@@ -17,7 +17,8 @@ function(test_add name)
         INVERT_RESULT
         WARN_DISABLE
         SPIKE_FAILURE
-        ENABLE_C_EXT)
+        ENABLE_C_EXT
+        ENABLE_MTE)
 
     set(options_one_value
         TICKS_TIMEOUT)
@@ -25,20 +26,14 @@ function(test_add name)
     cmake_parse_arguments(PARSE_ARGV 1 TEST_DESCR "${options_no_value}" "${options_one_value}" "")
 
     set(RTL_TEST_NAME "rtl_${name}")
-    set(RTL_SEC_STACK_TEST_NAME "rtl_sec_stack_${name}")
     set(SIM_TEST_NAME "sim_${name}")
-    set(SIM_SEC_STACK_TEST_NAME "sim_sec_stack_${name}")
 
     set(TEST_DIR_RTL "${CMAKE_BINARY_DIR}/tests/${RTL_TEST_NAME}")
-    set(TEST_DIR_RTL_SEC_STACK "${CMAKE_BINARY_DIR}/tests/${RTL_SEC_STACK_TEST_NAME}")
     set(TEST_DIR_SIM "${CMAKE_BINARY_DIR}/tests/${SIM_TEST_NAME}")
-    set(TEST_DIR_SIM_SEC_STACK "${CMAKE_BINARY_DIR}/tests/${SIM_SEC_STACK_TEST_NAME}")
 
     # This is a hack. Stupid cmake won't create working directories if not exist
     file(MAKE_DIRECTORY "${TEST_DIR_RTL}")
-    file(MAKE_DIRECTORY "${TEST_DIR_RTL_SEC_STACK}")
     file(MAKE_DIRECTORY "${TEST_DIR_SIM}")
-    file(MAKE_DIRECTORY "${TEST_DIR_SIM_SEC_STACK}")
 
     if (${TEST_DESCR_INVERT_RESULT})
         set(RINVERT "--driver-invert-result")
@@ -60,50 +55,28 @@ function(test_add name)
         set(TEST_TICKS_TIMEOUT "--ticks-timeout=${TEST_DESCR_TICKS_TIMEOUT}")
     endif()
 
-    if ("${name}" MATCHES "^c_.*")
-        if (NOT ${TEST_DESCR_DISABLE_SECURITY})
-            set(ENABLE_STACK_TAG "--enable-stack-tagging")
-        endif()
-    endif()
-
-    if (ENABLE_STACK_TAG)
-        add_test(NAME "${RTL_SEC_STACK_TEST_NAME}"
-            COMMAND "${TEST_RUNNER}" ${TEST_DESCR_UNPARSED_ARGUMENTS}
-                    ${RINVERT} ${NSC} ${NOWARN} ${ENABLE_C} ${TEST_TICKS_TIMEOUT} ${ENABLE_STACK_TAG}
-                    WORKING_DIRECTORY "${TEST_DIR_RTL_SEC_STACK}")
-
-        add_test(NAME "${SIM_SEC_STACK_TEST_NAME}"
-            COMMAND "${TEST_RUNNER}" ${TEST_DESCR_UNPARSED_ARGUMENTS}
-                    ${RINVERT} ${NSC} ${NOWARN} ${ENABLE_C} ${TEST_TICKS_TIMEOUT} ${ENABLE_STACK_TAG}
-                    --spike
-                    WORKING_DIRECTORY "${TEST_DIR_SIM_SEC_STACK}")
+    if (${TEST_DESCT_ENABLE_MTE})
+        set(ENABLE_MTE "--enable-stack-tagging")
     endif()
 
     add_test(NAME "${RTL_TEST_NAME}"
         COMMAND "${TEST_RUNNER}" ${TEST_DESCR_UNPARSED_ARGUMENTS}
-                ${RINVERT} ${NSC} ${NOWARN} ${ENABLE_C} ${TEST_TICKS_TIMEOUT}
+                ${RINVERT} ${NSC} ${NOWARN} ${ENABLE_C} ${TEST_TICKS_TIMEOUT} ${ENABLE_MTE}
                 WORKING_DIRECTORY "${TEST_DIR_RTL}")
 
     add_test(NAME "${SIM_TEST_NAME}"
         COMMAND "${TEST_RUNNER}" ${TEST_DESCR_UNPARSED_ARGUMENTS}
-                ${RINVERT} ${NSC} ${NOWARN} ${ENABLE_C} ${TEST_TICKS_TIMEOUT}
+                ${RINVERT} ${NSC} ${NOWARN} ${ENABLE_C} ${TEST_TICKS_TIMEOUT} ${ENABLE_MTE}
                 --spike
                 WORKING_DIRECTORY "${TEST_DIR_SIM}")
 
     if (${TEST_DESCR_NIGHTLY})
         set_tests_properties("${RTL_TEST_NAME}" PROPERTIES LABELS nightly)
         set_tests_properties("${SIM_TEST_NAME}" PROPERTIES LABELS nightly)
-        if (ENABLE_STACK_TAG)
-            set_tests_properties("${RTL_SEC_STACK_TEST_NAME}" PROPERTIES LABELS nightly)
-            set_tests_properties("${SIM_SEC_STACK_TEST_NAME}" PROPERTIES LABELS nightly)
-        endif()
     endif()
 
     if (${TEST_DESCR_SPIKE_FAILURE})
         set_tests_properties("${SIM_TEST_NAME}" PROPERTIES LABELS "nightly;sim_failure")
-        if (ENABLE_STACK_TAG)
-            set_tests_properties("${SIM_SEC_STACK_TEST_NAME}" PROPERTIES LABELS "nightly;sim_failure")
-        endif()
     endif()
 
 endfunction()
